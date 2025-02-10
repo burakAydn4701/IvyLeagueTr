8c1c11cb299878fd93a03fdcaf2f318fed9842bb
Burak Aydın <burakaydin@Buraks-MacBook-Air.local>
2025-02-10 17:24:30 +0300

    get-export-6

--- app/api/comments/[id]/replies/route.ts
+++ app/api/comments/[id]/replies/route.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { type NextApiRequest } from 'next';
 import connectDb from '@/lib/db';
 import Comment from '@/lib/models/comment';
 
 export async function GET(
-    request: Request | NextApiRequest,
+    request: Request | NextRequest,
     { params }: { params: Record<string, string> }
 ) {
     try {
